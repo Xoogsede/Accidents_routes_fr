2468fee9f88b26dc43cd389aea83e4c348ee5626
Xoogsede <abdi-jama.mahamoud@hotmail.fr>
2023-01-18 02:40:30 +0100

    modification du fichier setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,14 @@
-mkdir -p ~/.streamlit/echo "\
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"your-mail@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
 [server]\n\
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
 enableCORS = false\n\
 \n\
